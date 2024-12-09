67fafacbc96b1f27db8dd425bebd7011ec3b1279
AMeenalosini <69825070+AMeenalosini@users.noreply.github.com>
2024-12-09 10:11:20 -0800

    Add files via upload
    Javascript

---
+++ Block07-SecureVault/vault.js
@@ -0,0 +1,24 @@
+/**
+ * Pseudocode:
+ * 1. Arthimetic calculation for variable x
+ * 2. Arthimetic calculation for variable y
+ * 3. Arthimetic calculation for variable z
+ * 4. Create a string that says "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+ * 5. Display the combination on the HTML page or on alert pop with the text message
+ */
+
+//1. Arthimetic calculation for variable x
+const x = 5+5
+
+//2. Arthimetic calculation for variable y
+const y = 50-10
+
+//3. Arthimetic calculation for variable z
+const z = (39*39)/39
+
+//4. Create a string that says "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+const str="You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+
+//5. Display the combination on the HTML page or on alert pop with the text message
+alert(str+ x +" - " + y +" - "+ z);
+//alert('${str}${x} - ${y} - ${z}');
